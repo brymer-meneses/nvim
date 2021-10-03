d602e0afc8111eb7ea80dc020375773b53e5e53f
Brymer <meneses.brymer@gmail.com>
2021-10-03 21:22:14 +0800

    updates

--- lua/core/plugins/toggleterm.lua
+++ lua/core/plugins/toggleterm.lua
@@ -1,26 +1,26 @@
-require("toggleterm").setup {
-    size = 15,
-    open_mapping = [[<C-\>]],
-    hide_numbers = true, -- hide the number column in toggleterm buffers
-    shade_filetypes = {},
-    shade_terminals = true,
-    shading_factor = '1', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
-    insert_mappings = true,
-    start_in_insert = true,
-    persist_size = true,
-    direction = 'float',
-    close_on_exit = true, -- close the terminal window when the process exits
-    shell = vim.o.shell, -- change the default shell
-    -- This field is only relevant if direction is set to 'float'
-    float_opts = {
-        -- The border key is *almost* the same as 'nvim_win_open'
-        -- see :h nvim_win_open for details on borders however
-        -- the 'curved' border is a custom border type
-        -- not natively supported but implemented in this plugin.
-        border = 'curved',
-        width = 70,
-        height = 20,
-        winblend = 3,
-        highlights = {border = "Normal", background = "Normal"}
-    }
-}
+require("toggleterm").setup({
+	size = 20,
+	open_mapping = [[<C-\>]],
+	hide_numbers = true, -- hide the number column in toggleterm buffers
+	shade_filetypes = {},
+	shade_terminals = true,
+	shading_factor = "1", -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+	insert_mappings = true,
+	start_in_insert = true,
+	persist_size = true,
+	direction = "float",
+	close_on_exit = true, -- close the terminal window when the process exits
+	shell = vim.o.shell, -- change the default shell
+	-- This field is only relevant if direction is set to 'float'
+	float_opts = {
+		-- The border key is *almost* the same as 'nvim_win_open'
+		-- see :h nvim_win_open for details on borders however
+		-- the 'curved' border is a custom border type
+		-- not natively supported but implemented in this plugin.
+		border = "curved",
+		width = 125,
+		height = 25,
+		winblend = 3,
+		highlights = { border = "Normal", background = "Normal" },
+	},
+})
